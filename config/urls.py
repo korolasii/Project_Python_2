969f6444b5dc58a5fa1e88e61b8ed7a1553092bf
korolasii <timofei.kropachev@gmail.com>
2023-05-26 22:40:03 +0300

    homework_lesson_2

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import frontpage
+from core.views import frontpage, about, pricing, contacts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', frontpage)
+    path('', frontpage),
+    path('about.html/', about),
+    path('pricing.html/', pricing),
+    path('contacts.html/', contacts)
 ]
