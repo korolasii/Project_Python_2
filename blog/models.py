f86ecfe766ca52660cf1f0a30b4ce5db217f52b3
korolasii <timofei.kropachev@gmail.com>
2023-05-30 14:20:43 +0300

    lesson_3

--- blog/models.py
+++ blog/models.py
@@ -10,5 +10,19 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return f'{self.title}'
+    
+    class Meta:
+        verbose_name = 'Cтатья'
+        verbose_name_plural = 'Cтатьи'
+    
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f'{self.name}'
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
