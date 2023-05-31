7c032f358961bf33a3998c2118b49303cc2ade2b
korolasii <timofei.kropachev@gmail.com>
2023-05-31 19:06:14 +0300

    add teg and fix models

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Article, Category
+from .models import *
 # Register your models here.
-admin.site.register(Article)
-admin.site.register(Category)
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content', 'category__name']
+    list_display = ['title', 'category', 'created_at', 'updated_at']
+    list_filter = ['category', 'tags', 'created_at', 'updated_at']
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'slug']
+    list_filter = ['name']
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='articles', default=1, verbose_name='Категорія')
+    tags = models.ManyToManyField('Tag', related_name='articles', verbose_name='Теги')
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     content_preview = models.TextField()
@@ -16,9 +18,11 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-created_at']
     
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(verbose_name='URL', default='')
     
     def __str__(self):
         return f'{self.name}'
@@ -26,3 +30,13 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f'{self.name}'
+    
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
