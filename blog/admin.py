bd71105bcf4ef3bebc5ac621f1632458bed42f26
korolasii <timofei.kropachev@gmail.com>
2023-06-03 12:05:49 +0300

    created migration

--- blog/admin.py
+++ blog/admin.py
@@ -1,18 +1,34 @@
 from django.contrib import admin
-from .models import *
-# Register your models here.
 
+from .models import Article, Category, Tag , Comment
+
+class CommentItemInline(admin.TabularInline):
+    model = Comment
+    raw_id_fields = ['article']
+    
+# Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
     search_fields = ['title', 'content', 'category__name']
     list_display = ['title', 'category', 'created_at', 'updated_at']
     list_filter = ['category', 'tags', 'created_at', 'updated_at']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [
+        CommentItemInline
+    ]
 
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_display = ['name', 'slug']
     list_filter = ['name']
+    prepopulated_fields = {'slug': ('name',)}
+
+class CommentAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'email', 'content', 'article__title']
+    list_display = ['article','name', 'email', 'content', 'created_at']
+    list_filter = ['name', 'email', 'content', 'created_at']
 
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
